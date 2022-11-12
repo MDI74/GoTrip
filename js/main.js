0628c61a9564d618ce73d9a5907b9f37bf3cff4d
MDI74 <demon4301@gmail.com>
2022-11-12 19:51:29 +0500

    Адаптирована шапка и навигация
    Сделана меню навигации для телефонов, адаптировна шапка сайта

--- js/main.js
+++ js/main.js
@@ -6,9 +6,24 @@ const owl = $('.owl-carousel');
 owl.owlCarousel({
     center: true,
     loop: true,
-    margin: 30,
     startPosition: 1,
-    items: 3,
+    margin: 30,
+    items: 1,
+    responsive: {
+        460:{
+            items: 3,
+        },
+        850:{
+            items: 3,
+        },
+        1020:{
+            margin: 20,
+            items: 3,
+        },
+        1200:{
+            items: 3,
+        },
+    }
 });
 
 $('.slider__btn--prev').click(function() {
@@ -25,10 +40,15 @@ $('.slider__btn--next').click(function() {
 // Burger menu
 
 const navBurger = document.querySelector('.nav__burger');
+const nav = document.querySelector('nav')
 const menuIcon = document.querySelector('.menu-icon');
 
 navBurger.onclick = function(){
+    nav.classList.toggle('nav--mobile')
     menuIcon.classList.toggle('menu-icon-active');
+    document.body.classList.toggle('no-scroll');
 }
+
+
 
 
